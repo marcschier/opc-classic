b6e373025543c8b6a1103723d5861a93de2e7cc5
Marc Schier <marcschier@hotmail.com>
2019-04-01 16:02:07 +0200

    Restructure

---
+++ DotNet/Include/AssemblyVersionInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+internal static class AssemblyVersionInfo
+{
+    /// <summary>
+    /// The current copy right notice.
+    /// </summary>
+    public const string Copyright = "Copyright © 2003-2017 OPC Foundation, Inc";
+
+    /// <summary>
+    /// The current build file version.
+    /// </summary>
+    public const string CurrentFileVersion = "0.0";
+}
